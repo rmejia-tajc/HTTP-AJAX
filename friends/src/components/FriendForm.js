81bee102e73709ec27e7e20e13fb323beb8de47b
rmejia-tajc <rmejia.tajc@gmail.com>
2019-01-24 22:03:15 -0700

    complete project MVP

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,36 +1,47 @@
 import React from 'react';
 
 function FriendForm(props) {
+    function handleSubmit(event) {
+      event.preventDefault();
+      if (props.isUpdating) {
+        props.updateFriend();
+      } else {
+        props.addFriend();
+      }
+    }
     
 
     return (
         <div>
-            <h2>Add a Friend</h2>
-            <form>
+            <h2>{props.isUpdating ? 'Update Friend' : 'Add New Friend'}</h2>
+            <form onSubmit={handleSubmit}>
 
                 <input
-                    type="text"
-                    name="name"
-                    value="name"
-                    placeholder="NAME"
+                    type='text'
+                    name='name'
+                    value={props.friend.name}
+                    placeholder='NAME'
+                    onChange={props.handleChanges}
                 />
                 
                 <input
-                    type="number"
-                    name="age"
-                    value="age"
-                    placeholder="AGE"
+                    type='number'
+                    name='age'
+                    value={props.friend.age}
+                    placeholder='AGE'
+                    onChange={props.handleChanges}
                 />
                 
                 <input
-                    type="text"
-                    name="email"
-                    value="email"
-                    placeholder="EMAIL"
+                    type='text'
+                    name='email'
+                    value={props.friend.email}
+                    placeholder='EMAIL'
+                    onChange={props.handleChanges}
                 />
 
-                <button>
-                    ADD A FRIEND
+                <button type='submit'>
+                    {props.isUpdating ? 'Update Friend' : 'Add New Friend'}
                 </button>
 
             </form>
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function FriendList(props) {
     return (
         <div>
             {props.friends.map((friend) => (
-                <div key={friend.id}>
+                <Link to={`/List/${friend.id}`} key={friend.id} >
                     <h2>Name: {friend.name}</h2>
                     <p>Age: {friend.age}</p>
                     <p>email: {friend.email}</p>
-                </div>
+                </Link>
             ))}
         </div>
     )
